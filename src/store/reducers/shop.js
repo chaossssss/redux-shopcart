df46d93ebabd29f7c8f463700885b0091e092937
LAPTOP-UAONIT64\admin <1033729276@qq.com>
2021-12-27 10:53:56 +0800

    fix:修改state

--- src/store/reducers/shop.js
+++ src/store/reducers/shop.js
@@ -1,15 +1,25 @@
 const shops = (state = [], action) => {
   switch (action.type) {
     case 'ADD_GOOD':
-      return [
-        ...state,
-        {
-          id: action.id,
-          name: action.name,
-          price: action.price,
-          count: 1
-        }
-      ]
+      let result = state.findIndex(item => {
+        return item.id === action.id
+      })
+      if(result === -1) {
+        return [
+          ...state,
+          {
+            id: action.id,
+            name: action.name,
+            price: action.price,
+            count: 1
+          }
+        ]
+      }else {
+        let good = state[result]
+        good.count = good.count + 1
+        let newState = Object.assign({}, ...state, good)
+        return [newState]
+      }
     default:
       return state
   }
